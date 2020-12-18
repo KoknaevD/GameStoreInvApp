77d27d8d740203b4feb9d09a91203700614744df
KoknaevD <89024464050@yandex.ru>
2020-12-18 09:29:06 +0500

    rename to GameStore

--- app/src/main/java/com/udacity/gamestore/models/Game.kt
+++ app/src/main/java/com/udacity/gamestore/models/Game.kt
@@ -1,8 +1,8 @@
-package com.udacity.shoestore.models
+package com.udacity.gamestore.models
 
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Shoe(var name: String, var size: Double, var company: String, var description: String,
+data class Game(var name: String, var size: Double, var company: String, var description: String,
                 val images: List<String> = mutableListOf()) : Parcelable
--- app/src/test/java/com/udacity/gamestore/ExampleUnitTest.kt
+++ app/src/test/java/com/udacity/gamestore/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.udacity.shoestore
+package com.udacity.gamestore
 
 import org.junit.Test
 
