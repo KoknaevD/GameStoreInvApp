df395574df2eacbe6bb92a5b5628235ae7a5ce15
KoknaevD <89024464050@yandex.ru>
2020-12-22 18:29:52 +0500

    123

--- app/src/main/java/com/udacity/gamestore/MainActivity.kt
+++ app/src/main/java/com/udacity/gamestore/MainActivity.kt
@@ -2,19 +2,27 @@ package com.udacity.gamestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
-import timber.log.Timber
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.gamestore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityViewModel: ActivityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
 
         activityViewModel = ViewModelProvider(this).get(ActivityViewModel::class.java)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp()
+    }
 
 }
