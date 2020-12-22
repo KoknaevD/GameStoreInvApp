5f6a837175289a21a79593368d6704b9a725879d
koknaev <89024464050@yandex.ru>
2020-12-22 22:57:29 +0500

    recyclerview_game.xml

--- app/src/main/java/com/udacity/gamestore/games/GamesAdapter.kt
+++ app/src/main/java/com/udacity/gamestore/games/GamesAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.udacity.gamestore.R
-import com.udacity.gamestore.databinding.RecyclerviewGame2Binding
+import com.udacity.gamestore.databinding.RecyclerviewGameBinding
 
 
 class GamesAdapter(
@@ -17,21 +17,21 @@ class GamesAdapter(
         return games.size
     }
 
-    inner class GamesViewHolder(val recyclerviewGame2Binding: RecyclerviewGame2Binding) : RecyclerView.ViewHolder(recyclerviewGame2Binding.root) {
+    inner class GamesViewHolder(val recyclerviewGameBinding: RecyclerviewGameBinding) : RecyclerView.ViewHolder(recyclerviewGameBinding.root) {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         GamesViewHolder(
-            DataBindingUtil.inflate<RecyclerviewGame2Binding>(
+            DataBindingUtil.inflate<RecyclerviewGameBinding>(
                 LayoutInflater.from(parent.context),
-                R.layout.recyclerview_game2,
+                R.layout.recyclerview_game,
                 parent,
                 false
             )
         )
 
     override fun onBindViewHolder(holder: GamesViewHolder, position: Int) {
-        holder.recyclerviewGame2Binding.game = games[position]
+        holder.recyclerviewGameBinding.game = games[position]
     }
 
 }
--- app/src/main/java/com/udacity/gamestore/games/GamesViewModel.kt
+++ app/src/main/java/com/udacity/gamestore/games/GamesViewModel.kt
@@ -13,12 +13,49 @@ class GamesViewModel : ViewModel() {
 
     init {
         _games.value = arrayOf(
-            Game("Diablo", 10.50, "Blizzard", "a game", mutableListOf(), R.drawable.icon),
-            Game("World of Warcraft: Frozen throne", 15.50, "Blizzard", "a game", mutableListOf(R.drawable.hl_image)),
-            Game("Dota", 20.50, "Valve", "a game", mutableListOf(R.drawable.hl_image)),
-            Game("CS:GO", 25.50, "Valve", "a game", mutableListOf(R.drawable.hl_image)),
-            Game("HALF:LIFE", 30.50, "Valve", "a game", mutableListOf(R.drawable.hl_image)),
-            Game("Starcraft", 35.50, "Blizzard", "a game", mutableListOf(R.drawable.hl_image))
+            Game("Diablo",
+                10.50,
+                "Blizzard",
+                "a game",
+                mutableListOf(R.drawable.diablo_image)),
+
+            Game("World of Warcraft: Frozen throne",
+                15.50,
+                "Blizzard",
+                "a game",
+                mutableListOf(R.drawable.wow_image)),
+
+            Game("Dota",
+                20.50,
+                "Valve",
+                "a game",
+                mutableListOf(R.drawable.dota_image)),
+
+            Game("CS:GO",
+                25.50,
+                "Valve",
+                "a game",
+                mutableListOf(R.drawable.cs_image)),
+
+            Game("HALF:LIFE",
+                30.50,
+                "Valve",
+                "a game",
+                mutableListOf(R.drawable.hl_image)),
+
+            Game("Starcraft",
+                35.50,
+                "Blizzard",
+                "a game",
+                mutableListOf(R.drawable.starcraft_image)),
+
+            Game("New Game Test",
+                45900.50,
+                "test",
+                "VERY NICE GAME",
+                mutableListOf(R.drawable.image_available_soon))
+
+
         )
     }
 
